9cc0aa8ab94c7e1d13668da578e54a9ccc7da2db
Stefan Panitz <info@stefanpanitz.de>
2016-12-11 22:59:21 +0100

    handle undefined value

--- npv.js
+++ npv.js
@@ -5,11 +5,14 @@ const _ = require('lodash');
 const args = process.argv.slice(2);
 
 let property = 'version';
-
 if (args.length === 1) {
 	property = args[0];
 }
 
 const value = _.get(require(path.join(process.cwd(), 'package.json')), property);
+if (typeof value === 'undefined') {
+    process.stderr.write('Invalid property');
+    process.exit(1);
+}
 
 process.stdout.write(_.isString(value) ? value : JSON.stringify(value));
